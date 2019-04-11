c807f8e703e949634d883ecc33e75582a2a601fa
PharaohCola13 <spencer.riley@student.nmt.edu>
2019-04-11 11:12:46 -0600

    update

--- assets/js/jquery.particleground.js
+++ assets/js/jquery.particleground.js
@@ -184,7 +184,7 @@
     function Particle() {
       this.stackPos;
       this.active = true;
-      this.layer = Math.ceil(Math.random() * 3);
+      this.layer = Math.ceil(Math.random() * 4);
       this.parallaxOffsetX = 0;
       this.parallaxOffsetY = 0;
       // Initial particle position
@@ -370,20 +370,20 @@
 
   window[pluginName].defaults = {
     minSpeedX: 0.1,
-    maxSpeedX: 0.7,
+    maxSpeedX: 0.5,
     minSpeedY: 0.1,
-    maxSpeedY: 0.7,
-    directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+    maxSpeedY: 0.5,
+    directionX: 'left', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
     directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
     density: 10000, // How many particles will be generated: one particle every n pixels
     dotColor: '#666666',
     lineColor: '#666666',
-    particleRadius: 7, // Dot size
+    particleRadius: 10, // Dot size
     lineWidth: 1,
     curvedLines: false,
     proximity: 100, // How close two dots need to be before they join
     parallax: true,
-    parallaxMultiplier: 5, // The lower the number, the more extreme the parallax effect
+    parallaxMultiplier: 10, // The lower the number, the more extreme the parallax effect
     onInit: function() {},
     onDestroy: function() {}
   };
