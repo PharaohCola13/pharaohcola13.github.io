b97cbdc9d1f62fe6af9e1bd94dcce8d253d049d0
Spencer Riley <spencer.riley@student.nmt.edu>
2019-05-21 16:34:27 -0600

    Update tooltips.js

--- assets/js/tooltips.js
+++ assets/js/tooltips.js
@@ -1,8 +1,20 @@
 tippy('#skill-python', {
-    animateFill: false,
-    animation: 'fade',
-    duration: 0,
-    arrow: true,
-    interactive: true,
-    content: "Testing123"
+	animation: 'scale',
+	duration: 1000,
+	arrow: true,
+	interactive: true,
+	placement: 'left',
+	content: '<strong style="color: aqua">Proficiencies:</strong><br />' +
+	'<ul><li>GUI Development</li>' +
+	'<li>Data Analysis</li></ul>'
+})
+tippy('#skill-r', {
+	animation: 'scale',
+	duration: 1000,
+	arrow: true,
+	interactive: true,
+	placement: 'left',
+	content: '<strong style="color: aqua">Proficiencies:</strong><br />' +
+	'<ul><li>Nonlinear Regression</li>' +
+	'<li>Data Analysis</li></ul>'
 })
