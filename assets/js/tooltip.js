2b5b2b29784802c8de43f61ac032c2ffbb13b7fd
Spencer Riley <spencer.riley@student.nmt.edu>
2019-05-22 12:24:38 -0600

    Update and rename tooltips.js to tooltip.js

---
+++ assets/js/tooltip.js
@@ -0,0 +1,70 @@
+tippy('#skill-python', {
+	animation: 'scale',
+	duration: 1000,
+	arrow: true,
+	interactive: true,
+	placement: 'left',
+	theme: 'ocean',
+	content: '<strong>Proficiencies:</strong><br />' +
+	'<ul><li>GUI Development</li>' +
+	'<li>Data Analysis</li></ul>'
+})
+
+tippy('#skill-r', {
+	animation: 'scale',
+	duration: 1000,
+	arrow: true,
+	interactive: true,
+	placement: 'left',
+	theme: 'ocean',
+	content: '<strong>Proficiencies:</strong><br />' +
+	'<ul><li>Nonlinear Regression</li>' +
+	'<li>Data Analysis</li></ul>'
+})
+
+tippy("#geoexpanse", {
+	animation: 'scale',
+	duration: 1000,
+	arrow: true,
+	interactive: true,
+	placement: 'left',
+	theme: 'ocean',
+	content: "Travel to the Expanse"
+})
+
+tippy("#home", {
+    animation: 'scale',
+    duration: 1000,
+    arrow: true,
+    interactive: true,
+    placement: 'bottom',
+    theme: 'ocean',
+    content: "Home"
+})
+tippy("#mail", {
+    animation: 'scale',
+    duration: 1000,
+    arrow: true,
+    interactive: true,
+    placement: 'bottom',
+    theme: 'ocean',
+    content: "Shoot an Email"
+})
+tippy("#git", {
+    animation: 'scale',
+    duration: 1000,
+    arrow: true,
+    interactive: true,
+    placement: 'bottom',
+    theme: 'ocean',
+    content: "See some code"
+})
+tippy("#orcid", {
+    animation: 'scale',
+    duration: 1000,
+    arrow: true,
+    interactive: true,
+    placement: 'bottom',
+    theme: 'ocean',
+    content: "Look, more stuff"
+})
