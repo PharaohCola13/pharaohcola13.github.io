3096b6d2c28dcdefbe09af069df3b01002c296ac
Spencer Riley <spencer.riley@student.nmt.edu>
2021-10-03 17:55:36 -0600

    Update content.js

--- assets/js/content.js
+++ assets/js/content.js
@@ -7,7 +7,7 @@ $(function(){
         
         $('#edu-degree').html(Object.values(data.biography.education)[0].degree + ".")
 
-        $('#edu-focus-minor').html("Focus in " + Object.values(data.biography.education)[0].focus +". Minor in "+ data.biography.education[0].minor)
+        $('#edu-focus-minor').html("Focus in " + Object.values(data.biography.education)[0].focus +".\nMinor in "+ data.biography.education[0].minor)
         $('#edu-date').html(Object.values(data.biography.education)[0].date + " <em>(Expected)</em>.")
         $('#edu-place').html(Object.values(data.biography.education)[0].place)
 
